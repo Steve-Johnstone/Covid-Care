2c151aa4f01bf89b772cff171606b0ec6b62bce5
YuliaPichugina <t-ypichugina@hotels.com>
2021-09-29 17:45:05 +0100

    changes UI design for main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 
 const MainPage = () => {
     return (
-        <div className="container">
-            <h1>Covid Care</h1>
-            <h3>Main Page</h3>
-            <Link className="btn btn-primary mb-2" to="/volunteers/login">Login</Link>
+        <div style={{backgroundImage: "url(/backgroundImage.png)",
+        backgroundSize: "cover",
+        height: "100vh",
+    }}
+        >
+        <div 
+        className='flex'>
+            <h1>CovidCare</h1>
+            <p>Despite the success of the vaccination programme in the UK, Covid is still a major issue which is affecting many people every week, who are forced to self-isolate, either due to a positive test or because they have potentially been in contact with the virus.</p>
+            <p>CovidCare is a social enterprise, designed to connect those who need help, with volunteers in their local area, who are willing to assist them.</p>
+            <p>If you are self-isolating and need help with day-to-day chores, such as grocery shopping, picking up prescriptions or walking your dog, please register for help below.</p>
+            <p>If you are willing to offer your services to help people in your local area, please register as a volunteer below.</p>
+            </div>
+            <div 
+        className='footer'>
+            <Link style={{backgroundColor: '#2734AD'}} className="primaryButton" to="/volunteers/login">Login</Link>
             <br/>
-            <Link className="btn btn-primary" to="/volunteers/register">Register</Link>
-        </div>
+            <Link className="primaryButton" to="/volunteers/register">Register</Link>
+            </div>
+            </div>
     )
 }
 
